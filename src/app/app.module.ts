94e416f0d8b03d67537b3ae3a325a7188170e0cf
silviachaluisa <veronica-14-1998@hotmail.com>
2024-10-27 13:57:37 -0500

    Tab4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Importa el servicio File
+import { File } from '@ionic-native/file/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    File // Agrega File aquí dentro del único bloque providers
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { File } from '@ionic-native/file/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
-export class Tab4Page implements OnInit {
+export class Tab4Page {
+  textInput: string = '';
+  message: string = '';
 
-  constructor() { }
+  constructor(private file: File, private toastController: ToastController) {}
 
-  ngOnInit() {
+  async saveText() {
+    try {
+      // Ruta del archivo
+      const filePath = this.file.dataDirectory;
+      const fileName = 'texto.txt';
+
+      // Verifica si el archivo existe antes de escribir
+      const fileExists = await this.file.checkFile(filePath, fileName);
+      if (!fileExists) {
+        await this.file.createFile(filePath, fileName, true);
+      }
+
+      // Escribe el texto en el archivo
+      await this.file.writeFile(filePath, fileName, this.textInput, { replace: true });
+      this.message = `Texto guardado en: ${filePath}${fileName}`;
+      this.showToast('Texto guardado con éxito!');
+    } catch (error) {
+      console.error('Error:', error);
+      this.message = 'Error al guardar el texto.';
+      this.showToast('Error al guardar el texto.');
+    }
   }
 
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'top',
+    });
+    toast.present();
+  }
 }
