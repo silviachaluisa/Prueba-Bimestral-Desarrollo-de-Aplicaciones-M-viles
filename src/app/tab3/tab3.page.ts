ef6d19df6c8b1496dd17f1fdfbf867b3eaefa379
silviachaluisa <veronica-14-1998@hotmail.com>
2024-10-27 08:23:28 -0500

    Quinto Commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  a: number = 0;
+b: number = 0;
+c: number = 0;
+roots: any;
 
-  constructor() {}
+calculateRoots() {
+  const discriminant = Math.pow(this.b, 2) - 4 * this.a * this.c;
 
+  if (discriminant > 0) {
+    // Raíces reales distintas
+    const root1 = (-this.b + Math.sqrt(discriminant)) / (2 * this.a);
+    const root2 = (-this.b - Math.sqrt(discriminant)) / (2 * this.a);
+    this.roots = [root1, root2];
+  } else if (discriminant === 0) {
+    // Raíces reales iguales
+    const root = -this.b / (2 * this.a);
+    this.roots = [root, root];
+  } else {
+    // Raíces complejas
+    const realPart = -this.b / (2 * this.a);
+    const imaginaryPart = Math.sqrt(-discriminant) / (2 * this.a);
+    this.roots = [`${realPart} + ${imaginaryPart}i`, `${realPart} - ${imaginaryPart}i`];
+  }
+}
 }
